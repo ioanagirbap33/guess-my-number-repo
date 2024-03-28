efbe02daaed302d0c936b58b3ce84449e71313fd
ioana.girba <>
2024-03-28 10:36:43 +0200

    Styling buttons container

--- guess-my-number/App.js
+++ guess-my-number/App.js
@@ -3,12 +3,7 @@ import { StyleSheet, View } from "react-native";
 import { StartGameScreen } from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View>
-      <StartGameScreen />
-      <View></View>
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({});
